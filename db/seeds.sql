77bab79761f06c655b1e0ee7ac1fb7f07f7c11ea
mgreer973 <mgreer973@yahoo.com>
2016-05-13 18:25:34 -0400

    add all files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('cheese burger');
+INSERT INTO burgers (burger_name) VALUES ('hamburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger with onions', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheese with onions', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('bacon burger', true);
+INSERT INTO burgers (burger_name) VALUES ('bacon cheeseburger');
